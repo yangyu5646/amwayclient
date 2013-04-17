55562321209d56b1c519f573ec316e93ef0b5d27
yangyuqi <512707877@qq.com>
2013-04-17 22:46:37 +0800

    等等

--- src/com/bigboss/amway/MainActivity.java
+++ src/com/bigboss/amway/MainActivity.java
@@ -10,8 +10,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-    
-    
+           
     }
 
 
